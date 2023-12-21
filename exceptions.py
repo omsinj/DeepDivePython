d896d38b88b1f7bdf9851779578f9554de24c9c6
Omar J <100746749+omsinj@users.noreply.github.com>
2023-12-21 14:49:29 +0000

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -25,3 +25,24 @@ except KeyboardInterrupt:
     # Omitting an exception handles all unhandled exceptions.
 except:
     print('catch all without binding exception')
+
+def summarize_monthly():
+    path = Path(__file__).parent / 'payments.json'
+    try:
+        with open(path) as p:
+            payments = deserialize(p.read())
+
+            return { month: sum(payment) for month, payment in payments['months'].items() }
+    except KeyError as ex:
+        raise BillingSummaryError('unexpected file format.') from None
+    except FileNotFoundError:
+        raise BillingSummaryError('missing required file.') from None
+    except ValueError:
+        raise BillingSummaryError('non-numeric payment amount.') from None
+    except Exception as ex:
+        raise BillingSummaryError('unplanned exception.') from None
+
+
+
+
+
